75becdaa88ac3f6302aae6ed7e86dec8332707df
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-12-08 16:38:24 +0100

    Changed API route from 'api/SensorData' to 'api/data'

--- Backend/AtomicSensors/Controllers/SensorDataController.cs
+++ Backend/AtomicSensors/Controllers/SensorDataController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AtomicSensors.Controllers
 {
     [Controller]
-    [Route("api/[controller]")]
+    [Route("api")]
     public class SensorDataController: Controller
     {
         private readonly MongoDBService _mongoDBService;
@@ -14,7 +14,7 @@ namespace AtomicSensors.Controllers
             _mongoDBService = mongoDBService;
         }
 
-        [HttpGet]
+        [HttpGet("data")]
         public async Task<List<SensorData>> Get(string sort_mode, string sort_by, int id_filter, string type_filter, string date_filter)
         {
             return await _mongoDBService.GetAsync(sort_mode, sort_by, id_filter, type_filter, date_filter);
