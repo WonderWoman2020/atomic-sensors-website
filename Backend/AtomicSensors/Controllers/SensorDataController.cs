eaff6b6be6fd4a49e09e5926bfb63e0cf0dc78d2
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2023-12-08 22:07:54 +0100

    Added GET method for JSON download with api 'api/data/json' (same as normal Get method actually)

--- Backend/AtomicSensors/Controllers/SensorDataController.cs
+++ Backend/AtomicSensors/Controllers/SensorDataController.cs
@@ -22,5 +22,15 @@ namespace AtomicSensors.Controllers
                 $"Date from: {date_from}, Date to: {date_to}");
             return await _mongoDBService.GetAsync(sort_mode, sort_by, id_filter, type_filter, date_from, date_to);
         }
+
+        [HttpGet("data/json")]
+        public async Task<List<SensorData>> GetDownloadJson(string? sort_mode, string? sort_by, int? id_filter, string? type_filter, 
+            DateTime? date_from, DateTime? date_to)
+        {
+            Console.WriteLine($"Sort mode: {sort_mode}, Sort by: {sort_by}, Id filter: {id_filter}, Type filter: {type_filter}, " +
+                $"Date from: {date_from}, Date to: {date_to}");
+            return await _mongoDBService.GetAsync(sort_mode, sort_by, id_filter, type_filter, date_from, date_to);
+        }
+
     }
 }
